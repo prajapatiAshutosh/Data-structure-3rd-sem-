a95be45fbe1fc5b01265e16059f76f6e80347aaf
ASHUTOSH PRAJAPATI <114522500+prajapatiAshutosh@users.noreply.github.com>
2022-12-14 19:24:33 +0530

    Update linkedlist.cpp

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
 
-
 using namespace std;
 template <typename T>
 
 class Node
 {
 public:
-     Node<T> *next;
-     T data;
+    Node<T> *next;
+    T data;
     Node<T>(T val, Node<T> *ptr = NULL)
     {
         data = val;
         next = ptr;
     }
-    template<typename U>
+    template <typename U>
     friend class list;
 };
- 
- template<typename T>
+
+template <typename T>
 class list
 {
     Node<T> *head;
@@ -30,7 +29,7 @@ public:
     }
     bool isempty()
     {
-        return head==NULL;
+        return head == NULL;
     }
     void addhead(T data)
     {
@@ -59,14 +58,12 @@ public:
         if (head)
         {
             Node<T> *temp = head;
-            while (temp!= NULL)
+            while (temp != NULL)
             {
                 if (temp->data == ele)
                 {
                     Node<T> *n = new Node(val, temp->next);
-                    temp->next=n;
-                    
-                
+                    temp->next = n;
                 }
                 temp = temp->next;
             }
@@ -91,7 +88,7 @@ public:
             }
         }
     }
-    void addend(int val)
+    void addend(float val)
     {
         if (head)
         {
@@ -135,11 +132,10 @@ public:
         Node<T> *temp1 = head->next;
         if (head)
         {
-            while (temp->next!= NULL)
+            while (temp->next != NULL)
             {
-                
 
-                while (temp1!= NULL)
+                while (temp1 != NULL)
                 {
                     if (temp->data > temp1->data)
                     {
@@ -156,188 +152,183 @@ public:
             }
         }
     }
-    void deletefromend(){
-        if(head==NULL)
-         return ;
-        if(head->next==NULL){
-         delete head;
-        //   return ;
-        }
-           Node<T>* secondlast=head;
-           while(secondlast->next->next!=NULL){
-            secondlast=secondlast->next;
-           }
-           delete secondlast->next;
-           secondlast->next=NULL;
-           return;
-    }
-    list join(list l){
-        Node<T>* temp=l.head;
-        while(temp->next!=NULL)
+    void deletefromend()
+    {
+        if (head == NULL)
+            return;
+        if (head->next == NULL)
         {
-            temp=temp->next;
+            delete head;
+            //   return ;
         }
-        temp->next=this->head;
+        Node<T> *secondlast = head;
+        while (secondlast->next->next != NULL)
+        {
+            secondlast = secondlast->next;
+        }
+        delete secondlast->next;
+        secondlast->next = NULL;
+        return;
+    }
+    list join(list l)
+    {
+        Node<T> *temp = l.head;
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = this->head;
         // l.dispaly();
         return l;
     }
-    void insertafterindex(int x,T val ){
-        Node<T>* temp=head;
-        for(int i=0;i<x;i++){
-            temp=temp->next;
+    void insertafterindex(int x, T val)
+    {
+        Node<T> *temp = head;
+        for (int i = 0; i < x; i++)
+        {
+            temp = temp->next;
         }
-        insertafter(val,temp->data);
+        insertafter(val, temp->data);
     }
-  Node<T>* HEAD(){
-    return head;
-  }
-  int lengthoflist(){
-    int count=0;
-    Node<T>*temp=head;
-    while(temp){
-        count++;
-        temp=temp->next;
+    Node<T> *HEAD()
+    {
+        return head;
     }
-    return count;
-  }
-  void reverse(){
-    Node<T> *c=head;
-    Node<T> *p=NULL;
-    Node<T> *n;
+    int lengthoflist()
+    {
+        int count = 0;
+        Node<T> *temp = head;
+        while (temp)
+        {
+            count++;
+            temp = temp->next;
+        }
+        return count;
+    }
+    void reverse()
+    {
+        Node<T> *c = head;
+        Node<T> *p = NULL;
+        Node<T> *n;
 
-    while(c!=NULL){
-        n=c->next;
-        c->next=p;
-        p=c;
-        c=n;
-    }
-    head=p;
-  }
-};
-template<typename T>
-T palindrome(list<char> s){
-    int count=s.lengthoflist();
-    char* arr=new char[count];
-    int i=0,flag;
-    Node<char>* temp=s.HEAD();
-    while(temp){
-      arr[i]=temp->data;
-      temp=temp->next;
-      i++;
-    }
-    for(int i=0;i<count;i++){
-        if(arr[i]==arr[count-i-1]){
-            flag=0;
+        while (c != NULL)
+        {
+            n = c->next;
+            c->next = p;
+            p = c;
+            c = n;
         }
-        else{
-            flag=1;
+        head = p;
+    }
+};
+template <typename T>
+T palindrome(list<char> s)
+{
+    int count = s.lengthoflist();
+    char *arr = new char[count];
+    int i = 0, flag;
+    Node<char> *temp = s.HEAD();
+    while (temp)
+    {
+        arr[i] = temp->data;
+        temp = temp->next;
+        i++;
+    }
+    for (int i = 0; i < count; i++)
+    {
+        if (arr[i] == arr[count - i - 1])
+        {
+            flag = 0;
+        }
+        else
+        {
+            flag = 1;
             break;
         }
     }
-    if(flag==0){
- cout<<"List is palindrome ";
-
+    if (flag == 0)
+    {
+        cout << "List is palindrome ";
     }
     else
-    cout<<"Not Palindrome";
+        cout << "Not Palindrome";
 }
 
 int main()
 {
-   cout<<"==========MENU==========\n";
-   cout<<" 1.Insert  the value at head\n";
-   cout<<" 2. Insert after certain value\n";
-   cout<<" 3.Remove from Head\n";
-   cout<<" 4.Search the element\n";
-   cout<<" 5.Add at the end\n";
-   cout<<" 6.Remove from end\n";
-   cout<<" 7.Sort the linked list\n";
-   cout<<" 8.Join two linked list\n";
-   cout<<" 9.Reverse the linked list\n";
-   cout<<" 10.Check Palindrome Linked list";
+    list<float> l;
+    int choice;
+    char z, s;
 
-   list<float> l;
-   int choice;
-   cout<<"\n Enter the choice : ";
-   cin>>choice;
-   cout<<endl;
-   switch(choice){
-    case 1:do{
-           cout<<"\nEnter the value you want to enter : ";
-           int x;
-           cin>>x;
-           l.addhead(x);
-         cout<<"\nDo you want to Insert more(Y/N) : ";
-         char z;
-         cin>>z;
-           
-    }while(z=='Y' or z=='y');
-           break;
+    do
+    {
+        cout << "==========MENU==========\n";
+        cout << " 1.Insert  the value at head\n";
+        cout << " 2. Insert after certain value\n";
+        cout << " 3.Remove from Head\n";
+        cout << " 4.Search the element\n";
+        cout << " 5.Add at the end\n";
+        cout << " 6.Remove from end\n";
+        cout << " 7.Sort the linked list\n";
+        cout << " 8.Reverse the linked list\n";
+        cout << "\n Enter the choice : ";
+        cin >> choice;
+        cout << endl;
+        switch (choice)
+        {
+        case 1:
+            do
+            {
+                cout << "\nEnter the value you want to enter : ";
+                int x;
+                cin >> x;
+                l.addhead(x);
+                cout << "\nDo you want to Insert more(Y/N) : ";
 
-   }
-   
+                cin >> z;
 
+            } while (z == 'Y' or z == 'y');
+            break;
+
+        case 2:
+            cout << "Enter the element after which you want to enter";
+            int x1;
+            cin >> x1;
+            cout << "Enter the number you want to enter : ";
+            int y1;
+            cin >> y1;
+            l.insertafter(y1, x1);
+            break;
+        case 3:
+            cout << "Removed elemnt from head ";
+            l.removehead();
+            break;
+        case 4:
+            cout << "Enter the element you want to search : ";
+            int x2;
+            cin >> x2;
+            cout << "\nThe number is at :" << l.search(x2);
+            break;
+        case 5:
+            cout << "Enter the element  at the end : ";
+            int x3;
+            cin >> x3;
+            l.addend(x3);
+            break;
+        case 6:
+            cout << "Remove from the end ";
+            l.deletefromend();
+            break;
+        case 7:
+            l.sort1();
+            break;
+        case 8:
+            l.reverse();
+            break;
+        }
+
+        cout << "Do you want to continue (Y/N)";
+        cin >> s;
+    } while (s == 'y' || s == 'Y');
 }
 
-
-
-
-
-
-//     list <float> l;
-    
-//     l.addhead(1);
-//     l.addhead(1);
-//     l.addhead(2);
-//     // l.addhead(59.87);
-//     l.addhead(1);
-//     // l.addhead(41);
-//     // l.addhead(10);
-//     // l.insertafter(12, 59);
-//     // l.removehead();
-//     // l.dispaly();
-//     // l.reverse();
-//     // cout<<endl;
-//     // l.dispaly();
-//     // cout << "\nThe number is at position : " << l.search(12) << endl;
-//     // l.addend(13);
-//     // l.dispaly();
-//     // cout << endl;
-//     // l.removeend();
-//     // l.dispaly();
-//     // l.sort1();
-//     // cout << endl;
-//     // l.deletefromend();
-//     // l.dispaly();
-//     list<float> l2;
-//     l2.addhead(9);
-//     l2.addhead(19);
-//     l2.addhead(79);
-//     l2.addhead(99);
-//     cout<<endl;
-//     l2.dispaly();
-//     cout<<endl;
-//     list<float> l3;
-//     l3=l.join(l2);
-
-//     l3.dispaly();
-//     l3.insertafterindex(2,0.98);
-//     cout<<endl;
-//     l3.dispaly();
-//     list<char> s;
-//     s.addhead('1');
-//     s.addhead('1');
-//     s.addhead('2');
-//     s.addhead('1');
-//     // s.addhead('a');
-//     cout<<endl;
-//    palindrome<void>(s);
-//     cout<<endl;
-//       s.dispaly();
-//     //   Node<char>* m=s.HEAD();
-//     //   cout<<endl;
-//     //   cout<<m->data;
-//     // l.sort1();
-//     // l.dispaly();
-
-//     return 0;
